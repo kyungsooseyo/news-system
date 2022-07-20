705646596e7da34a7bb66f9415e96add3bbe8d84
wanglimin47 <wanglimin47@jd.com>
2022-07-20 19:45:22 +0800

    feat:添加json-server

--- src/components/SendBox/SideMenu.jsx
+++ src/components/SendBox/SideMenu.jsx
@@ -1,44 +1,70 @@
 import React, { useState } from 'react'
 import { Layout, Menu } from 'antd';
 import './sideMenu.scss'
-import { UserOutlined, VideoCameraOutlined, UploadOutlined } from '@ant-design/icons';
+import { UserOutlined, VideoCameraOutlined, UploadOutlined, QqOutlined } from '@ant-design/icons';
+import { withRouter } from 'react-router-dom';
 const { Sider } = Layout;
-export default function SideMenu() {
+function SideMenu(props) {
+  // console.log('pppp', props);
   const [collapsed, setCollapsed] = useState(false);
+  const renderMenu = () => {
+    return [
+      {
+        label: '首页',
+        icon: <UserOutlined />,
+        key: '/home',
+        children: [
+          {
+            label: '首页2',
+            key: '/home2',
+            icon: <UserOutlined />
+          }
+        ]
+      },
+      {
+        label: '视频',
+        icon: <VideoCameraOutlined />,
+        key: '/video'
+      },
+      {
+        label: '上传',
+        icon: <UploadOutlined />,
+        key: '/upload'
+      },
+      {
+        key: '/right-manage',
+        label: '权限管理',
+        icon: <UserOutlined />,
+        children: [
+          {
+            label: '用户列表',
+            key: '/right-manage/user/list',
+            icon: <QqOutlined />
+          },
+          {
+            label: '角色列表',
+            key: '/right-manage/role/list',
+            icon: <UserOutlined />
+          }
+        ]
+      }
+    ]
+  }
   return (
     <Sider trigger={null} collapsible collapsed={collapsed} className='side-menu-container'>
-      <div className="logo" >new system</div>
+      <div className="logo">new system</div>
       <Menu
         theme="dark"
         mode="inline"
         defaultSelectedKeys={['1']}
-        items={[
-          {
-            key: '1',
-            icon: <UserOutlined />,
-            label: '首页',
-            children: [
-              {
-                key: '1-1',
-                label: '首页2',
-                to: '/home3',
-                icon: <VideoCameraOutlined />,
-              }
-            ]
-          },
-          {
-            key: '2',
-            icon: <VideoCameraOutlined />,
-            label: 'nav 2',
-          },
-          {
-            key: '3',
-            icon: <UploadOutlined />,
-            label: 'nav 3',
-          },
-        ]}
-
+        //` 函数式编程
+        items={renderMenu()}
+        onClick={(e) => {
+          props.history.push(e.key)
+        }}
       />
     </Sider>
   )
 }
+//= 父级没给传 只能靠withRouter包一下
+export default withRouter(SideMenu)
--- src/views/NewsSandBox/Home/Home.jsx
+++ src/views/NewsSandBox/Home/Home.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import { Button } from 'antd';
+import axios from 'axios';
 export default function Home() {
+  const getData = (e) => {
+    // . 原生参数的 e 会默认传递过来
+    // axios.get('http://localhost:11111/comments', {
+    //   params: {
+    //     id: 1
+    //   }
+    // }).then(res => {
+    //   console.log('res', res)
+    // })
+    // - post在restful风格的接口中，默认就是增加
+    // axios({
+    //   method: 'post',
+    //   url: 'http://localhost:11111/posts',
+    //   data: {
+    //     title: 'title',
+    //     body: '111',
+    //   }
+    // })
+    // ! put方法弊端,会把整个对象都传递过去,其他的东西也会被替换掉
+    // axios.put('http://localhost:11111/posts/1', {
+    //   title: 'title-fix',
+    // })
+    // + patch方法就只会更改当前的值,其他的不会改变
+    // axios.patch('http://localhost:11111/posts/2', {
+    //   title: 'title-fix2',
+    // })
+    // - delete方法就是删除 在comments中 添加一个postId 与posts的id一致,那么删除的时候就会把两个表中的数据都删除掉
+    // axios.delete('http://localhost:11111/posts/7')
+    //- 关联表查询 通过_embed进行关联查询,在comments中必须以后postId才可进行关联查询
+    // axios.get('http://localhost:11111/posts?_embed=comments').then(res=>{
+    //   console.log(res.data);
+    // })
+    //- _expand 是反查用的，必须在每个comments中都得有postId才可以进行反查,少一个就会报错
+    axios.get('http://localhost:11111/comments?_expand=post').then(res => {
+      console.log(res.data);
+    })
+  }
   return (
     <div>Home
-      <Button type="primary">Primary</Button>
+      {/* 要想给函数传递参数，就必须使用箭头函数的形式，如果不传参的话那么直接写上函数名字就行 */}
+      {/* <Button type="primary" onClick={() => getData(1)}>Primary</Button> */}
+      <Button type="primary" onClick={getData}>Primary</Button>
     </div>
   )
 }
