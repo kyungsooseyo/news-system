a512248227b3352f8654f3505b0c268dc8c12c62
wanglimin47 <wanglimin47@jd.com>
2022-07-28 19:38:15 +0800

    feat:sideMenu完成、引入table

--- src/components/SendBox/SideMenu.jsx
+++ src/components/SendBox/SideMenu.jsx
@@ -1,68 +1,121 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Layout, Menu } from 'antd';
 import './sideMenu.scss'
-import { UserOutlined, VideoCameraOutlined, UploadOutlined, QqOutlined } from '@ant-design/icons';
+import { UserOutlined, VideoCameraOutlined, UploadOutlined, QqOutlined, TableOutlined } from '@ant-design/icons';
 import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 const { Sider } = Layout;
 function SideMenu(props) {
   // console.log('pppp', props);
   const [collapsed, setCollapsed] = useState(false);
-  const renderMenu = () => {
-    return [
-      {
-        label: '首页',
-        icon: <UserOutlined />,
-        key: '/home',
-        children: [
-          {
-            label: '首页2',
-            key: '/home2',
-            icon: <UserOutlined />
-          }
-        ]
-      },
-      {
-        label: '视频',
-        icon: <VideoCameraOutlined />,
-        key: '/video'
-      },
-      {
-        label: '上传',
-        icon: <UploadOutlined />,
-        key: '/upload'
-      },
-      {
-        key: '/right-manage',
-        label: '权限管理',
-        icon: <UserOutlined />,
-        children: [
-          {
-            label: '用户列表',
-            key: '/right-manage/user/list',
-            icon: <QqOutlined />
-          },
-          {
-            label: '角色列表',
-            key: '/right-manage/role/list',
-            icon: <UserOutlined />
-          }
-        ]
-      }
-    ]
+  const [menu, setMenu] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:11111/rights?_embed=children').then(res => {
+      console.log('sideMenu', res.data)
+      setMenu(res.data)
+    })
+    return () => {
+    }
+  }, [])
+  const iconList = {
+    '/home': <UserOutlined />,
+    '/right-manage': <VideoCameraOutlined />,
+    '/user-manage': <UploadOutlined />,
+    '/news-manage': <QqOutlined />,
+    '/audit-manage': <TableOutlined />,
   }
+  const TreeDataSource = (arr) => {
+    if (!Array.isArray(arr)) {
+      return;
+    }
+    //` 不需要完全复制,只需要一些关键key label 就行
+    return arr.filter(item => item?.pagepermisson === 1).map((v, i) => {
+      if (v.children) {
+        return {
+          // ...v,
+          id: v.id,
+          key: v.key,
+          label: v.title,
+          grade: v.grade,
+          icon: iconList[v.key],
+          children: TreeDataSource(v.children.filter(item => item?.pagepermisson === 1)),
+        };
+      } else {
+        return {
+          // ...v,
+          id: v.id,
+          key: v.key,
+          icon: iconList[v.key],
+          grade: v.grade,
+          label: v.title,
+        };
+      }
+    });
+  }
+  const renderMenu = () => {
+    // return [
+    //   {
+    //     label: '首页',
+    //     icon: <UserOutlined />,
+    //     key: '/home',
+    //     children: [
+    //       {
+    //         label: '首页2',
+    //         key: '/home2',
+    //         icon: <UserOutlined />
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     label: '视频',
+    //     icon: <VideoCameraOutlined />,
+    //     key: '/video'
+    //   },
+    //   {
+    //     label: '上传',
+    //     icon: <UploadOutlined />,
+    //     key: '/upload'
+    //   },
+    //   {
+    //     key: '/right-manage',
+    //     label: '权限管理',
+    //     icon: <UserOutlined />,
+    //     children: [
+    //       {
+    //         label: '用户列表',
+    //         key: '/right-manage/user/list',
+    //         icon: <QqOutlined />
+    //       },
+    //       {
+    //         label: '角色列表',
+    //         key: '/right-manage/role/list',
+    //         icon: <UserOutlined />
+    //       }
+    //     ]
+    //   }
+    // ]
+    return TreeDataSource(menu)
+  }
+  console.log(props);
+  const selectMenu = props.location.pathname
+  const openKeys =  props.location.pathname.split('/')[1]
   return (
     <Sider trigger={null} collapsible collapsed={collapsed} className='side-menu-container'>
-      <div className="logo">new system</div>
-      <Menu
-        theme="dark"
-        mode="inline"
-        defaultSelectedKeys={['1']}
-        //` 函数式编程
-        items={renderMenu()}
-        onClick={(e) => {
-          props.history.push(e.key)
-        }}
-      />
+
+      <div className="menu-wrapper">
+        <div className="logo">new system</div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          selectedKeys={[selectMenu]}
+          defaultOpenKeys={[`/${openKeys}`]}
+          //` 函数式编程
+          items={renderMenu()}
+          onClick={(e) => {
+            props.history.push(e.key)
+          }}
+        />
+      </div>
     </Sider>
   )
 }
--- src/views/NewsSandBox/RightManage/RightList.jsx
+++ src/views/NewsSandBox/RightManage/RightList.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { Table } from 'antd'
+import axios from 'axios'
 export default function RightList() {
+
+  useEffect(() => {
+    axios.get('http://localhost:11111/rights').then(res => {
+      console.log('rights', res.data)
+      setDataSource(res.data)
+    })
+  }, [])
+  const [dataSource, setDataSource] = useState([])
+  //~ dataIndex 相当于element的prop
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      // key: 'name',
+    },
+    {
+      title: '权限名称',
+      dataIndex: 'title',
+    },
+    {
+      title: '权限路径',
+      dataIndex: 'key',
+    },
+  ];
+
   return (
-    <div>RightList</div>
+    <div>
+      <Table dataSource={dataSource} columns={columns}></Table>
+
+    </div>
   )
 }
